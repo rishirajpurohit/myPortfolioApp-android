f81c2fe3732401a1eb00d22a0955e73c380a6d61
Rishiraj Purohit <purohitji007@gmail.com>
2016-07-19 05:46:01 +0530

    initial commit of myPortfolioApp

---
+++ app/src/main/java/in/rishirajpurohit/android/myportfolioapp/myPortfolioApp.java
@@ -0,0 +1,13 @@
+package in.rishirajpurohit.android.myportfolioapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class myPortfolioApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_portfolio_app);
+    }
+}
