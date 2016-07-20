343ff2dd351b1d8a8e471b391831e1e7321043f0
Rishiraj Purohit <purohitji007@gmail.com>
2016-07-20 19:36:03 +0530

    app final code ready with all toast added

--- app/src/main/java/in/rishirajpurohit/android/myportfolioapp/myPortfolioApp.java
+++ app/src/main/java/in/rishirajpurohit/android/myportfolioapp/myPortfolioApp.java
@@ -2,6 +2,9 @@ package in.rishirajpurohit.android.myportfolioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class myPortfolioApp extends AppCompatActivity {
 
@@ -10,4 +13,24 @@ public class myPortfolioApp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_portfolio_app);
     }
+
+    public void btn_popularMovies_clicked(View view){
+        Toast.makeText(myPortfolioApp.this, "This will open POPULAR MOVIES App ", Toast.LENGTH_SHORT).show();
+    }
+    public void btn_stockHawk_clicked(View view){
+        Toast.makeText(myPortfolioApp.this, "This will open STOCK HAWK App ", Toast.LENGTH_SHORT).show();
+    }
+    public void btn_buildItBigger_clicked(View view){
+        Toast.makeText(myPortfolioApp.this, "This will open BUILD IT BIGGER App ", Toast.LENGTH_SHORT).show();
+    }
+    public void btn_makeAppMaterial_clicked(View view){
+        Toast.makeText(myPortfolioApp.this, "This will open MAKE YOUR APP MATERIAL App ", Toast.LENGTH_SHORT).show();
+    }
+    public void btn_goUbiquitous_clicked(View view){
+        Toast.makeText(myPortfolioApp.this, "This will open GO UBIQUITOUS App ", Toast.LENGTH_SHORT).show();
+    }
+    public void btn_capstone_clicked(View view){
+        Toast.makeText(myPortfolioApp.this, "This will open CAPSTONE App ", Toast.LENGTH_SHORT).show();
+    }
+
 }
