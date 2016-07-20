48103ccffeb59eb4a5b9c618cdbdedcb377afa6d
Rishiraj Purohit <purohitji007@gmail.com>
2016-07-20 19:52:08 +0530

    Final Version Done

--- app/src/main/java/in/rishirajpurohit/android/myportfolioapp/myPortfolioApp.java
+++ app/src/main/java/in/rishirajpurohit/android/myportfolioapp/myPortfolioApp.java
@@ -2,6 +2,7 @@ package in.rishirajpurohit.android.myportfolioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.style.BackgroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,6 +13,7 @@ public class myPortfolioApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_portfolio_app);
+
     }
 
     public void btn_popularMovies_clicked(View view){
